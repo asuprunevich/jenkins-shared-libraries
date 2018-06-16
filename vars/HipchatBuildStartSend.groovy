b8719c279e50eeb980db8bd754036835cb76c6ae
Alexander Suprunevich <asuprunevich@gorillagroup.com>
2018-06-16 14:03:31 +0200

    hipchat

--- vars/HipchatBuildStartSend.groovy
+++ vars/HipchatBuildStartSend.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String CredKeyId, int RoomId) {
+def call(String CredKeyId) {
 
 	hipchatSend(
 		color: 'YELLOW',
 		credentialId: "${CredKeyId}",
 		message: 'Build has been started',
 		notify: true,
-		room: "${RoomId}",
+		room: '4048687',
 		sendAs: 'JenkinsSharedLibrary',
 		server: 'api.hipchat.com',
 		v2enabled: true
